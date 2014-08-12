c7d50f3d9eef92a77b4d849ee9adad376709a75e
Rani Itani <itani.rani@gmail.com>
2014-08-12 14:49:16 -0400

    elsif exercise

--- exercise3.rb
+++ exercise3.rb
@@ -1,9 +1,24 @@
-year = 2014
-puts "what's your name?"
-name = gets.chomp
-puts "Your name is #{name}!"
+# year = 2014
+# puts "what's your name?"
+# name = gets.chomp
+# puts "Your name is #{name}!"
 
-puts "What's your age?"
-age = gets.chomp
+# puts "What's your age?"
+# age = gets.chomp
 
-puts "You were born in #{year - age.to_i}" 
+# puts "You were born in #{year - age.to_i}" 
+
+# if true 
+# 	puts " I get printed"
+# end 
+
+x= 3
+y= 4
+
+if x >y
+	puts "x is greater than y!"
+elsif x<y
+	puts "x is less than y!"
+else 
+	puts "x equals y!"
+end 
