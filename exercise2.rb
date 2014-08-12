8814a2ab4863a8fa63ea5fb14eb605bd74cce8a1
Rani Itani <itani.rani@gmail.com>
2014-08-12 14:07:14 -0400

    booleans registry

--- exercise2.rb
+++ exercise2.rb
@@ -1,11 +1,13 @@
-a =  55
-b = 0.25
+# a =  55
+# b = 0.25
 
-puts  "A good tip for a 55 dollar is #{a * b} "
+# puts  "A good tip for a 55 dollar is #{a * b} "
 
-#puts "hello" + 6  <-- you would get an error
+# #puts "hello" + 6  <-- you would get an error
 
-a = 45628
-b = 7839
+# a = 45628
+# b = 7839
 
-puts "ex2-3 result of multiplication is #{a * b}"
+# puts "ex2-3 result of multiplication is #{a * b}"
+
+puts (true && false) || (false && true) || !(false && false)
