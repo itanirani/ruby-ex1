91bd0200fb5127dc220a40c053dac3c31311a124
Rani Itani <itani.rani@gmail.com>
2014-08-12 13:57:53 -0400

    ruby ex2 try 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,13 @@
-puts 2 
-puts 3 
-puts 2 != 3
+# puts 2 
+# puts 3 
+# puts 2 != 3
+
+a  = 20
+b = 65
+puts "the value of a is #{a}." # The value of a is 20.
+puts "The value of a is #{b}." # The value of b is 65
+puts "the value of a plus b is #{a + b}." # The value of a plus b is 85.
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\b\bGoodbye world"
+puts "1. Hello\n2. World"
---
+++ exercise2.rb
@@ -0,0 +1,4 @@
+a =  55
+b = 0.25
+
+puts  "The total including tip is #{a * b} "
